310838bcee6b3f3eda9cfec9daca55063f81a3aa
beth-em <beth.murillo318@gmail.com>
2025-04-21 20:03:57 -0600

    push again...

---
+++ Develop/dist/server.js
@@ -0,0 +1,44 @@
+import express from 'express';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { ApolloServer } from 'apollo-server-express';
+import { typeDefs, resolvers } from './schemas/index.js';
+import db from './config/connection.js';
+import { getUserFromToken } from './services/auth.js';
+import dotenv from 'dotenv';
+dotenv.config();
+const PORT = process.env.PORT || 3001;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+// Apollo Server setup
+async function startApolloServer() {
+    const app = express();
+    app.use(express.urlencoded({ extended: true }));
+    app.use(express.json());
+    const server = new ApolloServer({
+        typeDefs,
+        resolvers,
+        context: ({ req }) => {
+            // middleware to inject user into context for resolvers
+            const authHeader = req.headers.authorization || '';
+            const token = authHeader.split(' ')[1];
+            const user = getUserFromToken(token);
+            return { user };
+        },
+    });
+    await server.start();
+    server.applyMiddleware({ app: app });
+    // if we're in production, serve client/build as static assets
+    if (process.env.NODE_ENV === 'production') {
+        app.use(express.static(path.join(__dirname, '../client/dist')));
+        app.get('*', (_req, res) => {
+            res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
+        });
+    }
+    db.once('open', () => {
+        app.listen(PORT, () => {
+            console.log(`🌍 Now listening on localhost:${PORT}${server.graphqlPath}`);
+        });
+    });
+}
+startApolloServer();
